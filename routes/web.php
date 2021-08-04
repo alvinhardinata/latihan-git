baf4fc7a7bdabffb986c49802de75a438f05707c
alvinhardinata <you@example.com>
2021-08-04 13:17:37 +0700

    membuat route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('halo', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about' , [
+        "nama" => "Alvin" ,
+        "email" => "alvin@gmail.com" ,
+        "image" => "gambar hp.jpg"
+    ]);
 });
